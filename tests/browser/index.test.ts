ebe8bbd6367673043577091becc00efe5e0ff152
Sylvain Lesage <severo@rednegra.net>
2025-11-05 11:25:38 +0100

    simpler way of sharing tests between node and browser

--- tests/browser/index.test.ts
+++ tests/browser/index.test.ts
@@ -1,4 +1,9 @@
 import { test, expect } from '@playwright/test'
-import { launch } from '../common/index.test'
+import { tests } from '../common/index.test'
 
-launch({ expect, test })
+for (const { name, prepare, expected } of tests) {
+  test(name, async () => {
+    const result = await prepare()
+    expect(result).toBe(expected)
+  })
+}
--- tests/node/index.test.ts
+++ tests/node/index.test.ts
@@ -1,4 +1,9 @@
 import { expect, test } from 'vitest'
-import { launch } from '../common/index.test'
+import { tests } from '../common/index.test'
 
-launch({ expect, test })
+for (const { name, prepare, expected } of tests) {
+  test(name, async () => {
+    const result = await prepare()
+    expect(result).toBe(expected)
+  })
+}
