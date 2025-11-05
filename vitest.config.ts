a65e871b2be984b5de747a27d8da2c066d9c7b2b
Sylvain Lesage <severo@rednegra.net>
2025-11-05 14:40:35 +0100

    run node and browser tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,17 +3,31 @@ import { playwright } from '@vitest/browser-playwright'
 
 export default defineConfig({
   test: {
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      headless: true,
-      // https://vitest.dev/guide/browser/playwright
-      instances: [
-        { browser: 'chromium' },
-        { browser: 'firefox' },
-        // webkit is not working on my machine
-        // { browser: 'webkit' },
-      ],
-    },
+    projects: [
+      {
+        test: {
+          name: 'node',
+          environment: 'node',
+        },
+      },
+      {
+
+        test: {
+          name: 'browser',
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            // https://vitest.dev/guide/browser/playwright
+            instances: [
+              { browser: 'chromium' },
+              { browser: 'firefox' },
+              // webkit is not working on my machine
+              // { browser: 'webkit' },
+            ],
+          },
+        },
+      },
+    ],
   },
 })
