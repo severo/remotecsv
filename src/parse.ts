eea480de50fca650b55d6c085e2da0b48d185337
Sylvain Lesage <severo@rednegra.net>
2025-11-06 11:22:36 +0100

    pass requestInit to parse function

--- src/parse.ts
+++ src/parse.ts
@@ -8,21 +8,23 @@ import { fetchChunk } from './fetch'
  * @param options Options for parsing.
  * @param options.chunkSize The size of each chunk to fetch. Default is 1MB.
  * @param options.fileSize The total size of the file in bytes. If not provided, it will be determined from the first chunk.
+ * @param options.requestInit Optional fetch request initialization parameters.
  * @returns An async generator that yields chunks of text.
  */
 export async function* parse(
   url: string,
-  options: {
+  options?: {
     chunkSize?: number
     fileSize?: number
+    requestInit?: RequestInit
   },
 ): AsyncGenerator<{
   text: string
   offset: number
   byteCount: number
 }> {
-  const chunkSize = setChunkSize(options.chunkSize)
-  let fileSize = options.fileSize
+  const chunkSize = setChunkSize(options?.chunkSize)
+  let fileSize = options?.fileSize
   const decoder = new TextDecoder('utf-8')
 
   let rangeStart = 0
@@ -31,7 +33,7 @@ export async function* parse(
     const extraByte = 1
     const rangeEnd = rangeStart + chunkSize - 1 + extraByte
 
-    const result = await fetchChunk({ url, rangeStart, rangeEnd })
+    const result = await fetchChunk({ url, rangeStart, rangeEnd, requestInit: options?.requestInit })
 
     fileSize ??= result.fileSize
     const bytesToDecode = Math.min(chunkSize, fileSize - rangeStart)
