e8fa332b8550179923ad49f1f53a37e05d44c920
Sylvain Lesage <severo@rednegra.net>
2025-11-10 13:49:41 +0100

    add test file

---
+++ tests/quoteChar.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest'
+
+import { validateQuoteChar } from '../src/quoteChar'
+
+describe('validateQuoteChar', () => {
+  it('should return the default quote character when none is provided', () => {
+    expect(validateQuoteChar()).toBe('"')
+  })
+
+  it('should return the provided quote character', () => {
+    expect(validateQuoteChar('\'')).toBe('\'')
+    expect(validateQuoteChar('`')).toBe('`')
+  })
+})
