3aa9db2e71024fa8444c2f8e70f275210cdaff66
Sylvain Lesage <severo@rednegra.net>
2025-11-13 23:34:57 +0100

    add test with fromAsync

--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
 import { parseURL } from '../src'
+import type { ParseResult } from '../src/types'
 
 describe('README examples', () => {
   it('parses a remote CSV file', async () => {
@@ -8,6 +9,14 @@ describe('README examples', () => {
     for await (const { row } of parseURL('https://data.source.coop/severo/csv-papaparse-test-files/sample.csv')) {
       rows.push(row)
     }
+    expect(rows).toEqual([['A', 'B', 'C'], ['X', 'Y', 'Z']])
+  })
+  it('parses a remote CSV, and we can use Array.fromAsync for convenience', async () => {
+    // @ts-expect-error ts(2345) - fromAsync is available now
+    const rows = await Array.fromAsync(
+      parseURL('https://data.source.coop/severo/csv-papaparse-test-files/sample.csv'),
+      async (result: ParseResult) => result.row,
+    )
     expect(rows).toEqual([['A', 'B', 'C'], ['X', 'Y', 'Z']])
   })
 })
