d91fb27ceac6b568794586e00912efd103a69c54
Sylvain Lesage <severo@rednegra.net>
2025-11-13 14:45:15 +0100

    remove dead code

--- src/options/check.ts
+++ src/options/check.ts
@@ -16,21 +16,3 @@ export function checkIntegerGreaterOrEqualThan(value: number | undefined, minVal
   }
   throw new Error(`Invalid value: ${value}`)
 }
-
-/**
- * Throws if the provided value is not a non-negative integer. Undefined is allowed.
- * @param value The desired value.
- * @returns The validated value: a non-negative integer, or undefined.
- */
-export function checkNonNegativeInteger(value?: number): number | undefined {
-  if (
-    value === undefined
-    || (
-      Number.isInteger(value)
-      && value >= 0
-    )
-  ) {
-    return value
-  }
-  throw new Error(`Invalid value: ${value}`)
-}
