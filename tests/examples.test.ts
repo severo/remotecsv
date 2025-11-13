84091c05e088674765eabe2e17120450c34dce22
Sylvain Lesage <severo@rednegra.net>
2025-11-13 23:39:18 +0100

    remove test with fromAsync

--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
 import { parseURL } from '../src'
-import type { ParseResult } from '../src/types'
 
 describe('README examples', () => {
   it('parses a remote CSV file', async () => {
@@ -11,12 +10,13 @@ describe('README examples', () => {
     }
     expect(rows).toEqual([['A', 'B', 'C'], ['X', 'Y', 'Z']])
   })
-  it('parses a remote CSV, and we can use Array.fromAsync for convenience', async () => {
-    // @ts-expect-error ts(2345) - fromAsync is available now
-    const rows = await Array.fromAsync(
-      parseURL('https://data.source.coop/severo/csv-papaparse-test-files/sample.csv'),
-      async (result: ParseResult) => result.row,
-    )
-    expect(rows).toEqual([['A', 'B', 'C'], ['X', 'Y', 'Z']])
-  })
+  // fromAsync is not available yet in CI node.js version... Letting the code for reference
+  // it('parses a remote CSV, and we can use Array.fromAsync for convenience', async () => {
+  //   // @ts-expect-error ts(2345) - fromAsync is available now
+  //   const rows = await Array.fromAsync(
+  //     parseURL('https://data.source.coop/severo/csv-papaparse-test-files/sample.csv'),
+  //     async (result: ParseResult) => result.row,
+  //   )
+  //   expect(rows).toEqual([['A', 'B', 'C'], ['X', 'Y', 'Z']])
+  // })
 })
